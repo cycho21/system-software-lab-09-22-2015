2b58de7eb8d9b244d09b3101bf96e90c6d050334
lovebube@home <lovebube@hotmail.com>
2015-09-21 22:05:29 +0900

    simple project for class 09-22-2015

---
+++ src/kr/ac/uos/lab/software/system/Application.java
@@ -0,0 +1,42 @@
+package kr.ac.uos.lab.software.system;
+
+/**
+ * @author Chan Yeon, Cho
+ * @link http://github.com/lovebube
+ * @since 2015-09-21
+ */
+
+public class Application {
+
+    private Person p1;
+    private Person p2;
+    private Person p3;
+    private int personCount;
+
+    public Application() {
+        personCount = 0;
+        startApplication();
+    }
+
+    private void startApplication() {
+        setPersons();
+        getAverageAge();
+    }
+
+    private void setPersons() {
+        p1 = new Person("Jae Hyung", "Captain", 30);
+        personCount++;
+        p2 = new Person("Yoon Gwan", "Spy", 29);
+        personCount++;
+        p3 = new Person("Yeon Jung", "God", 25);
+        personCount++;
+    }
+
+    private void getAverageAge() {
+        System.out.println("Average age of this people is " + (p1.getAge() + p2.getAge() + p3.getAge())/personCount + " years");
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
+}
---
+++ src/kr/ac/uos/lab/software/system/Person.java
@@ -0,0 +1,52 @@
+package kr.ac.uos.lab.software.system;
+
+/**
+ * @author Chan Yeon, Cho
+ * @link http://github.com/lovebube
+ * @since 2015-09-21
+ */
+
+public class Person {
+
+    private String name;
+    private String job;
+    private int age;
+
+    public Person(String name, String job, int age) {
+        this.name = name;
+        this.job = job;
+        this.age = age;
+        printInfo();
+    }
+
+    private void printInfo() {
+        System.out.println("Name : " + this.name);
+        System.out.println("Job  : " + this.job);
+        System.out.println("Age  : " + this.age);
+        System.out.println("******************");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
